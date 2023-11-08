14386b304bb8cfa5360f9ba6a0a368c359612e79
unknown <forkan.saadaoui@gmail.com>
2023-11-08 10:06:07 +0100

    remove unused code

--- client/src/pages/edit-image/EditImage.jsx
+++ client/src/pages/edit-image/EditImage.jsx
@@ -6,77 +6,68 @@ import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
 
 function EditImage() {
-    const user = JSON.parse(localStorage.getItem('user'))
-    const {photo} = useParams()
-    const [photos, setPhotos] = useState([])
-    const [updatedPhoto, setUpdatedPhoto] = useState({
-      title: "",
-      description: ""
-    })
-    const { title, description } = updatedPhoto
-    const onChange = (e) => {
-      setUpdatedPhoto({ ...updatedPhoto, [e.target.name]: e.target.value });
-    };
-    const navigate = useNavigate()
+  const user = JSON.parse(localStorage.getItem("user"));
+  const { photo } = useParams();
+  const [photos, setPhotos] = useState([]);
+  const [updatedPhoto, setUpdatedPhoto] = useState({
+    title: "",
+    description: "",
+  });
+  const { title, description } = updatedPhoto;
+  const onChange = (e) => {
+    setUpdatedPhoto({ ...updatedPhoto, [e.target.name]: e.target.value });
+  };
+  const navigate = useNavigate();
 
-    /**
-     * Get The Current Image
-     */
-    useEffect(() => {
-        myImages();
-      }, []);
-    
-      const myImages = async () => {
-        await axios
-          .get(`/api/images/user_images`, {
-            headers: {
-              "Content-Type": "application/json",
-              "x-auth-token": user?.data.token,
-            },
-          })
-          .then((res) => {
-            setPhotos(res?.data);
-          })
-          .catch((err) => console.error(err.response.data.msg));
-      };
-    const current_photo = photos.find(p=>p._id === photo)
-    const imageSrc = current_photo?.photo
+  /**
+   * Get The Current Image
+   */
+  useEffect(() => {
+    myImages();
+  }, []);
+
+  const myImages = async () => {
+    await axios
+      .get(`/api/images/user_images`, {
+        headers: {
+          "Content-Type": "application/json",
+          "x-auth-token": user?.data.token,
+        },
+      })
+      .then((res) => {
+        setPhotos(res?.data);
+      })
+      .catch((err) => console.error(err.response.data.msg));
+  };
+  const current_photo = photos.find((p) => p._id === photo);
+  const imageSrc = current_photo?.photo
     ? require(`../../assets/images/${current_photo.photo}`)
     : null;
 
-    /**
-     * Update Image Informations
-     */
-    const updateImg = (e) => {
-        e.preventDefault()
-        axios
-        .put(`/api/images/user_images/${current_photo._id}`, updatedPhoto, {
-          headers: {
-            "Content-Type": "application/json",
-            "x-auth-token": user.data?.token,
-          },
-        })
-        .then((res) => {
-          setUpdatedPhoto(res.data) 
-          navigate('/')
-        })
-        .catch((error) => console.error(error));
-      
-    }
+  /**
+   * Update Image Informations
+   */
+  const updateImg = (e) => {
+    e.preventDefault();
+    axios
+      .put(`/api/images/user_images/${current_photo._id}`, updatedPhoto, {
+        headers: {
+          "Content-Type": "application/json",
+          "x-auth-token": user.data?.token,
+        },
+      })
+      .then((res) => {
+        setUpdatedPhoto(res.data);
+        navigate("/");
+      })
+      .catch((error) => console.error(error));
+  };
 
-    
   return (
-    <div className='share-image'>
-      <HeaderProfile
-        title="Edit Image"
-        paragraphe=""
-      />
-      <Navbar/>
+    <div className="share-image">
+      <HeaderProfile title="Edit Image" paragraphe="" />
+      <Navbar />
       <ShareImageComponent
-        // imageTitle={title}
-        // setImageTitle={settitle}
-        // imageDescription={description}
-        // setImageDescription={setdescription}
         image={imageSrc}
         show={false}
         postImage={updateImg}
